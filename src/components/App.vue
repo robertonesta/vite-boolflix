ede597430ea5f3ede445cb171499ac2d4a4bfad2
RobertoNesta <robertonesta1993@gmail.com>
2023-04-19 11:47:38 +0200

    created appheader, appmain and appfooter components

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from "./AppHeader.vue"
+import AppMain from "./AppMain.vue"
+import AppFooter from "./AppFooter.vue"
+
+</script>
+
+<template>
+    <AppHeaderVue/>
+    <AppMain/>
+    <AppFooter/>
+    
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: AppHeader,
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
